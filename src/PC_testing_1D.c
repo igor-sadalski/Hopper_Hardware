e441f00ed162a737a6eb74c8132ad0cfd4ed24f7
Igor Sadalski <63819680+igor-sadalski@users.noreply.github.com>
2022-08-05 10:37:59 -0700

    Sending/Receiving only one message at the time

--- src/PC_testing_1D.c
+++ src/PC_testing_1D.c
@@ -4,11 +4,31 @@
 #include <string.h>
 #include <sys/socket.h>
 
-#include <PC_testing_fun.h>
-
 #define MAX 80
 #define PORT 8888
 #define SA struct sockaddr
+
+void func(int sockfd)
+{
+	char buff[MAX];
+	int n;
+	for (;;) {
+		bzero(buff, sizeof(buff));
+		n = 0;
+		while ((buff[n++] = getchar()) != '\n')
+			;
+		write(sockfd, buff, sizeof(buff));
+		bzero(buff, sizeof(buff));
+		read(sockfd, buff, sizeof(buff));
+		printf("From Server : %s", buff);
+		if ((strncmp(buff, "exit", 4)) == 0) {
+			printf("Client Exit...\n");
+			break;
+		}
+	}
+}
+
+
 int main()
 {
 	int sockfd, connfd;
