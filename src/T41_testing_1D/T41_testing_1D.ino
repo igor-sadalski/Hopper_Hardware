d7f0a2bcb0050ce382b3044005cd0b4f2b95398b
Igor Sadalski <63819680+igor-sadalski@users.noreply.github.com>
2022-08-05 14:07:00 -0700

    Sending Unrecognized double to Pc
    Only one string without formating

--- src/T41_testing_1D/T41_testing_1D.ino
+++ src/T41_testing_1D/T41_testing_1D.ino
@@ -1,33 +1,14 @@
-/* 
- *  Sergio Esteban (sesteban@cpp.edu)
- *  To be uploaded to Teensy 4.1
- *  Send a double and receive it back
- *  Use RX/TX 1
- *  UPLOAD TO TEENSY
- *  CHANGE: tools>>board>>teensyuduino>>teensy4.1
- *  THIS WILL GO THE THE KOIOS_MVP program
- */
+const unsigned int MAX_MESSAGE_LENGTH = 50;
 
 void setup() {
-  // Begin the Serial at 115200 Baud
-  Serial.begin(115200);
-  Serial1.begin (115200);
-
-  // Give time to setup Serial
+  delay(100); //give time to open and write
+  Serial.begin(115200); //this is for the monitor
+  Serial1.begin(115200); //chekc if Serial1 baud is the same!
   delay(100);
-
-  // Turn LED on to indicate that Teensy is on
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
 }
 
-void loop(){
-  
-  Serial.println("-----------------------------------------");
-  
-  // choose double to send
-  double num = 3.141592653589793238;
-  //double num = Serial.read();
+void loop() {
+ double num = 3.141592653589793238;
 
   // create pointer with 8 bytes of double's memory
   byte * num_bytes = (byte *) &num;
@@ -43,16 +24,11 @@ void loop(){
   // Some delimeter to distinguish what part of buffer you are on
   Serial1.write(delimiter);
 
+
   // Send all 8 bytes of double to Serial buffer
   int i = 0;
   while (i < sizeof(num)) {
     Serial1.write(num_bytes[i]);
     i++;
   }
-
-  Serial.print("Serial 1 just sent double: ");
-  Serial.println(num,20);
-  
-  delay(1000);
-  
-} 
+}
