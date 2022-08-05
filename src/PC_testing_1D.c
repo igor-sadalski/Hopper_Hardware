d3ea94dc70b179aa9e809e864548a4653d0eb622
Igor Sadalski <63819680+igor-sadalski@users.noreply.github.com>
2022-08-05 15:55:57 -0700

    2D string communication working

--- src/PC_testing_1D.c
+++ src/PC_testing_1D.c
@@ -3,31 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+//#include <time.h>
 
 #define MAX 80
 #define PORT 8888
 #define SA struct sockaddr
-
-void func(int sockfd)
-{
-	char buff[MAX];
-	int n;
-	for (;;) {
-		bzero(buff, sizeof(buff));
-		n = 0;
-		while ((buff[n++] = getchar()) != '\n')
-			;
-		write(sockfd, buff, sizeof(buff));
-		bzero(buff, sizeof(buff));
-		read(sockfd, buff, sizeof(buff));
-		printf("From Server : %s", buff);
-		if ((strncmp(buff, "exit", 4)) == 0) {
-			printf("Client Exit...\n");
-			break;
-		}
-	}
-}
-
 
 int main()
 {
@@ -59,12 +39,25 @@ int main()
 
 	// function for chat
 	char buff[MAX];
+	char send_buff[10] = "<from PC>";
+	
+	//struct timespec tstart={0,0}, tend={0,0};
+	    
+	    
 	while(1){
+		//clock_gettime(CLOCK_MONOTONIC, &tstart);
+		
+		
 		bzero(buff, sizeof(buff));
 		read(sockfd, buff, sizeof(buff));
 		printf("%s \n", buff);
+		write(sockfd, send_buff, sizeof(send_buff));
 		
 		
+		//clock_gettime(CLOCK_MONOTONIC, &tend);
+	    //printf("some_long_computation took about %.5f seconds\n",
+           //((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - 
+           //((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
 	}
 
 	// close the socket
