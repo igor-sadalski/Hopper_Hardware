5da523834ad631aadeb55c494134da8c191e1522
Igor Sadalski <63819680+igor-sadalski@users.noreply.github.com>
2022-08-05 17:26:36 -0700

    Cleaned teensy code
    converted all to sending string in both directions, working with other test teensy

--- src/T41_testing_1D/T41_testing_1D.ino
+++ src/T41_testing_1D/T41_testing_1D.ino
@@ -1,34 +1,20 @@
-const unsigned int MAX_MESSAGE_LENGTH = 50;
-
 void setup() {
   delay(100); //give time to open and write
   Serial.begin(115200); //this is for the monitor
-  Serial1.begin(115200); //chekc if Serial1 baud is the same!
+  Serial1.begin(115200); //check if Serial1 baud is the same!
   delay(100);
 }
 
 void loop() {
- double num = 3.141592653589793238;
+  
+  //send string states, T41 -> ESP8266
+  Serial1.write("<from teens1y>");
 
-  // create pointer with 8 bytes of double's memory
-  byte * num_bytes = (byte *) &num;
-
-  // for sending a number, select indicators for bytes
-  char delimiter = ',';
-
-  // ================================================================================
-  //   SENDING DATA
-  // ================================================================================
-  // Start new double to send
-
-  // Some delimeter to distinguish what part of buffer you are on
-  Serial1.write(delimiter);
-
-
-  // Send all 8 bytes of double to Serial buffer
-  int i = 0;
-  while (i < sizeof(num)) {
-    Serial1.write(num_bytes[i]);
-    i++;
+   //read string torques, T41 <- ESP8266
+  while (Serial1.available() > 0) {
+          char c = Serial1.read();
+          Serial.write(c);
+        }
+  
+  delay(1000);
   }
-}
