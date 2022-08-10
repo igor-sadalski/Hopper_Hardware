eedb19fa7b3f5b9a8ba1a9b1735552ec769a3ee0
Igor Sadalski <63819680+igor-sadalski@users.noreply.github.com>
2022-08-09 18:45:55 -0700

    Minor updates, not working
    Sending string using the println command (which is better that serial.write()) and receiving using custom function for save specific portion of binary stream separated by <...>  into the receivedChars string (char array).

--- src/T41_testing_1D/T41_testing_1D.ino
+++ src/T41_testing_1D/T41_testing_1D.ino
@@ -1,20 +1,57 @@
+const byte numChars = 32;
+char receivedChars[numChars]; //change here to get more charactes in the string
+boolean newData = false;
+
 void setup() {
-  delay(100); //give time to open and write
+  delay(100); //give time to open and print
   Serial.begin(115200); //this is for the monitor
-  Serial1.begin(115200); //check if Serial1 baud is the same!
+  Serial1.begin(115200); //baud rates must be the same
   delay(100);
 }
 
 void loop() {
-  
-  //send string states, T41 -> ESP8266
-  Serial1.write("<from teens1y>");
+    Serial.println("Sending message to ESP/PC");
+    Serial1.println("<from teensy>");
+    Serial.println("Receiving from PC/ESP: "); ReadMessage(); 
+    if (newData == true) {
+       Serial.println(receivedChars);
+       newData = false;
+    }
+    delay(1000);
+}
 
-   //read string torques, T41 <- ESP8266
-  while (Serial1.available() > 0) {
-          char c = Serial1.read();
-          Serial.write(c);
+void ReadMessage() {
+/* 
+ *  save specific portion of binary stream separated by <...> 
+ *  into the receivedChars string (char array)
+*/
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '{'; //terminating conditions
+    char endMarker = '}'; //terminating conditions
+    char rc; //byte we are reading
+
+    while (Serial1.available() > 0 && newData == false) {
+        rc = Serial1.read(); //read in byte
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc; //add char that is in <...> to out string
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1; //if outside string size limit go back
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; //terminate that char
+                recvInProgress = false; //what we received is not "<"
+                ndx = 0;
+                newData = true;
+            }
         }
-  
-  delay(1000);
-  }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
