fd11d523549257bf50493bcb9ef2026b4ead6506
Igor Sadalski <63819680+igor-sadalski@users.noreply.github.com>
2022-08-10 14:08:02 -0700

    Working one way communication from PC to T41

--- src/T41_testing_1D/T41_testing_1D.ino
+++ src/T41_testing_1D/T41_testing_1D.ino
@@ -6,13 +6,16 @@ void setup() {
   delay(100); //give time to open and print
   Serial.begin(115200); //this is for the monitor
   Serial1.begin(115200); //baud rates must be the same
+  while (!Serial1) {;}
   delay(100);
 }
 
 void loop() {
-    Serial.println("Sending message to ESP/PC");
-    Serial1.println("<from teensy>");
-    Serial.println("Receiving from PC/ESP: "); ReadMessage(); 
+    Serial.println("----Sending message to ESP/Teensy----");
+    Serial.print("Is Serial1 ready:"); Serial.println(Serial1);
+    Serial1.println("<fromteensy>");
+    Serial.println("-----Receiving from PC/ESP:------ "); ReadMessage(); 
+    Serial.print("new data from ESP:"); Serial.println(Serial1.available());
     if (newData == true) {
        Serial.println(receivedChars);
        newData = false;
@@ -27,16 +30,16 @@ void ReadMessage() {
 */
     static boolean recvInProgress = false;
     static byte ndx = 0;
-    char startMarker = '{'; //terminating conditions
+    char startMarker = '{';
     char endMarker = '}'; //terminating conditions
     char rc; //byte we are reading
 
     while (Serial1.available() > 0 && newData == false) {
         rc = Serial1.read(); //read in byte
 
-        if (recvInProgress == true) {
+        if (recvInProgress == true) { //means we are in <...>
             if (rc != endMarker) {
-                receivedChars[ndx] = rc; //add char that is in <...> to out string
+                receivedChars[ndx] = rc; //add char to our string
                 ndx++;
                 if (ndx >= numChars) {
                     ndx = numChars - 1; //if outside string size limit go back
@@ -46,12 +49,12 @@ void ReadMessage() {
                 receivedChars[ndx] = '\0'; //terminate that char
                 recvInProgress = false; //what we received is not "<"
                 ndx = 0;
-                newData = true;
+                newData = true; //at end marker so finished reading a message
             }
         }
 
         else if (rc == startMarker) {
-            recvInProgress = true;
+            recvInProgress = true; //start logging from the next char
         }
     }
 }
