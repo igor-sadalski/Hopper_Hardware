7a2693e5e14cd8b56506a5a9d86fcf9e1d5f9a4e
igor <igor@Amber20.04>
2022-08-12 14:10:01 -0700

    sending torques works.

--- src/PC_testing_1D.c
+++ src/PC_testing_1D.c
@@ -8,10 +8,24 @@
 #define PORT 8888
 #define SA struct sockaddr
 
+void TokenizeStringToFloats(char str[], float states[]){
+    char * pch;
+    pch = strtok (str,",");
+    int i = 0;
+    while (pch != NULL)
+    {
+        states[i] = strtof (pch, NULL);
+        i++;
+        pch = strtok (NULL, ",");
+    }
+}
+
 int main()
 {
 	int sockfd, connfd;
 	struct sockaddr_in servaddr, cli;
+	//3
+	//float states[11]; //states + 1 
 
 	// socket create and verification
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -36,10 +50,13 @@ int main()
 	else
 		printf("connected to the server..\n");
 
-	// function for chat
 	char buff[MAX];
 	//cannot use spaces or special characters in the string message as the program otherwise freaks out
-	char send_buff[MAX] = "<{123.1234,1234.1234,134.1234,1234.1234}>"; //edit this to what the actual output buffor will be
+	//torques to currents, assumes torques is an array of floats
+	//3
+	//const_wheels = 0.083;
+	//sprintf(send_buff, "%f,%f,%f", torques[0]/const_wheels, torques[1]/const_wheels, torques[2]/const_wheels);
+	char send_buff[MAX] = "<{0,0.5,0}>"; //edit this to what the actual output buffor will be
 	        
 	while(1){
 		bzero(buff, sizeof(buff));
@@ -51,12 +68,13 @@ int main()
 		read(sockfd, buff, sizeof(buff));
 		printf("%s \n", buff);	
 		
-		//here will also go function to detokenize the string
-		//into set of controller usable floats
-		//its in libs in my github
+		//assume tokenization on ,
+		//3
+		//TokenizeStringToFloats(buff, states)
+		//send states further with ROS to the controller
 	}
 
 	close(sockfd);
-	
-	
 }
+
+//gcc PC_testing_1D.c -o pc_testing
